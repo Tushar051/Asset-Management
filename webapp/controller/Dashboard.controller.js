f90f3abc212692b1cbacf18dd4044c314ee2af61
Tushar <tusharkedar110@gmail.com>
2026-03-21 00:03:13 +0530

    Add comprehensive dashboard enhancements: KPI tiles, search, bulk actions, export, confirmations, and asset details

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -123,6 +123,331 @@ sap.ui.define([
             return aAssigned ? aAssigned.length : 0;
         },
 
+        getPendingCount: function (aRequests) {
+            return aRequests ? aRequests.length : 0;
+        },
+
+        onRefresh: function () {
+            MessageToast.show("Dashboard refreshed");
+            this.getModel().refresh(true);
+        },
+
+        onTotalAssetsTilePress: function () {
+            MessageToast.show("Total Assets: " + this.getTotalAssets(
+                this.getModel().getProperty("/availableAssets"),
+                this.getModel().getProperty("/assignedAssets")
+            ));
+        },
+
+        onAvailableAssetsTilePress: function () {
+            var oIconTabBar = this.byId("iconTabBar");
+            oIconTabBar.setSelectedKey("available");
+        },
+
+        onAssignedAssetsTilePress: function () {
+            var oIconTabBar = this.byId("iconTabBar");
+            oIconTabBar.setSelectedKey("assigned");
+        },
+
+        onSearchRequests: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oList = this.byId("requestsList");
+            var oBinding = oList.getBinding("items");
+            
+            if (sQuery) {
+                var aFilters = [
+                    new sap.ui.model.Filter({
+                        filters: [
+                            new sap.ui.model.Filter("employeeName", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("requestedAssetName", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("employeeId", sap.ui.model.FilterOperator.Contains, sQuery)
+                        ],
+                        and: false
+                    })
+                ];
+                oBinding.filter(aFilters);
+            } else {
+                oBinding.filter([]);
+            }
+        },
+
+        onSearchAvailable: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oList = this.byId("availableAssetsList");
+            var oBinding = oList.getBinding("items");
+            
+            if (sQuery) {
+                var aFilters = [
+                    new sap.ui.model.Filter({
+                        filters: [
+                            new sap.ui.model.Filter("assetName", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("category", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("description", sap.ui.model.FilterOperator.Contains, sQuery)
+                        ],
+                        and: false
+                    })
+                ];
+                oBinding.filter(aFilters);
+            } else {
+                oBinding.filter([]);
+            }
+        },
+
+        onSearchAssigned: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oList = this.byId("assignedAssetsList");
+            var oBinding = oList.getBinding("items");
+            
+            if (sQuery) {
+                var aFilters = [
+                    new sap.ui.model.Filter({
+                        filters: [
+                            new sap.ui.model.Filter("employeeName", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("assetName", sap.ui.model.FilterOperator.Contains, sQuery),
+                            new sap.ui.model.Filter("category", sap.ui.model.FilterOperator.Contains, sQuery)
+                        ],
+                        and: false
+                    })
+                ];
+                oBinding.filter(aFilters);
+            } else {
+                oBinding.filter([]);
+            }
+        },
+
+        onBulkApprove: function () {
+            var oList = this.byId("requestsList");
+            var aSelectedItems = oList.getSelectedItems();
+            
+            if (aSelectedItems.length === 0) {
+                MessageToast.show("Please select requests to approve");
+                return;
+            }
+            
+            this._showConfirmDialog(
+                "Approve Requests",
+                "Are you sure you want to approve " + aSelectedItems.length + " request(s)?",
+                "Accept",
+                "Accept",
+                "Warning",
+                function () {
+                    this._processBulkApproval(aSelectedItems);
+                }.bind(this)
+            );
+        },
+
+        onBulkReject: function () {
+            var oList = this.byId("requestsList");
+            var aSelectedItems = oList.getSelectedItems();
+            
+            if (aSelectedItems.length === 0) {
+                MessageToast.show("Please select requests to reject");
+                return;
+            }
+            
+            this._showConfirmDialog(
+                "Reject Requests",
+                "Are you sure you want to reject " + aSelectedItems.length + " request(s)?",
+                "Reject",
+                "Reject",
+                "Error",
+                function () {
+                    this._processBulkRejection(aSelectedItems);
+                }.bind(this)
+            );
+        },
+
+        _processBulkApproval: function (aSelectedItems) {
+            var oModel = this.getModel();
+            var aRequests = oModel.getProperty("/requests") || [];
+            var aAssigned = oModel.getProperty("/assignedAssets") || [];
+            
+            aSelectedItems.forEach(function (oItem) {
+                var oContext = oItem.getBindingContext();
+                var oRequest = oContext.getObject();
+                
+                var oNow = new Date();
+                var sDate = oNow.toISOString().split('T')[0];
+                var sTime = oNow.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+                
+                aAssigned.push({
+                    employeeName: oRequest.employeeName,
+                    assetName: oRequest.requestedAssetName,
+                    category: "Laptop",
+                    description: "Assigned based on request",
+                    assignedDate: sDate,
+                    assignedTime: sTime
+                });
+                
+                var iIndex = aRequests.indexOf(oRequest);
+                if (iIndex > -1) {
+                    aRequests.splice(iIndex, 1);
+                }
+            });
+            
+            oModel.setProperty("/requests", aRequests);
+            oModel.setProperty("/assignedAssets", aAssigned);
+            
+            this._addNotification(
+                "System",
+                aSelectedItems.length + " request(s) approved and assets assigned",
+                "sap-icon://accept"
+            );
+            
+            MessageToast.show(aSelectedItems.length + " request(s) approved successfully");
+            this.byId("requestsList").removeSelections(true);
+        },
+
+        _processBulkRejection: function (aSelectedItems) {
+            var oModel = this.getModel();
+            var aRequests = oModel.getProperty("/requests") || [];
+            
+            aSelectedItems.forEach(function (oItem) {
+                var oContext = oItem.getBindingContext();
+                var oRequest = oContext.getObject();
+                
+                var iIndex = aRequests.indexOf(oRequest);
+                if (iIndex > -1) {
+                    aRequests.splice(iIndex, 1);
+                }
+            });
+            
+            oModel.setProperty("/requests", aRequests);
+            
+            this._addNotification(
+                "System",
+                aSelectedItems.length + " request(s) rejected",
+                "sap-icon://decline"
+            );
+            
+            MessageToast.show(aSelectedItems.length + " request(s) rejected");
+            this.byId("requestsList").removeSelections(true);
+        },
+
+        _showConfirmDialog: function (sTitle, sMessage, sButtonText, sButtonType, sState, fnCallback) {
+            var oModel = this.getModel();
+            oModel.setProperty("/confirmTitle", sTitle);
+            oModel.setProperty("/confirmMessage", sMessage);
+            oModel.setProperty("/confirmButtonText", sButtonText);
+            oModel.setProperty("/confirmButtonType", sButtonType);
+            oModel.setProperty("/confirmState", sState);
+            this._confirmCallback = fnCallback;
+            
+            if (!this._confirmDialog) {
+                Fragment.load({
+                    id: this.getView().getId(),
+                    name: "admin.asset.manager.view.fragments.ConfirmDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    this._confirmDialog = oDialog;
+                    this.getView().addDependent(oDialog);
+                    oDialog.open();
+                }.bind(this));
+            } else {
+                this._confirmDialog.open();
+            }
+        },
+
+        onConfirmAction: function () {
+            if (this._confirmCallback) {
+                this._confirmCallback();
+            }
+            this._confirmDialog.close();
+        },
+
+        onCancelConfirm: function () {
+            this._confirmDialog.close();
+        },
+
+        onAssetDetailsPress: function (oEvent) {
+            var oItem = oEvent.getSource();
+            var oContext = oItem.getBindingContext();
+            var oAsset = oContext.getObject();
+            oAsset.status = "Available";
+            oAsset.statusState = "Success";
+            
+            this._showAssetDetails(oContext);
+        },
+
+        onAssignedAssetDetailsPress: function (oEvent) {
+            var oItem = oEvent.getSource();
+            var oContext = oItem.getBindingContext();
+            var oAsset = oContext.getObject();
+            oAsset.status = "Assigned";
+            oAsset.statusState = "Warning";
+            
+            this._showAssetDetails(oContext);
+        },
+
+        _showAssetDetails: function (oContext) {
+            if (!this._assetDetailsDialog) {
+                Fragment.load({
+                    id: this.getView().getId(),
+                    name: "admin.asset.manager.view.fragments.AssetDetailsDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    this._assetDetailsDialog = oDialog;
+                    this.getView().addDependent(oDialog);
+                    oDialog.bindElement(oContext.getPath());
+                    oDialog.open();
+                }.bind(this));
+            } else {
+                this._assetDetailsDialog.bindElement(oContext.getPath());
+                this._assetDetailsDialog.open();
+            }
+        },
+
+        onCloseAssetDetails: function () {
+            this._assetDetailsDialog.close();
+        },
+
+        onExportRequests: function () {
+            var aRequests = this.getModel().getProperty("/requests") || [];
+            if (aRequests.length === 0) {
+                MessageToast.show("No data to export");
+                return;
+            }
+            this._exportToCSV(aRequests, "Asset_Requests", ["employeeId", "employeeName", "requestedAssetName", "status"]);
+        },
+
+        onExportAvailable: function () {
+            var aAssets = this.getModel().getProperty("/availableAssets") || [];
+            if (aAssets.length === 0) {
+                MessageToast.show("No data to export");
+                return;
+            }
+            this._exportToCSV(aAssets, "Available_Assets", ["assetName", "category", "configuration", "description", "date"]);
+        },
+
+        onExportAssigned: function () {
+            var aAssets = this.getModel().getProperty("/assignedAssets") || [];
+            if (aAssets.length === 0) {
+                MessageToast.show("No data to export");
+                return;
+            }
+            this._exportToCSV(aAssets, "Assigned_Assets", ["employeeName", "assetName", "category", "description", "assignedDate", "assignedTime"]);
+        },
+
+        _exportToCSV: function (aData, sFileName, aColumns) {
+            var sCsv = aColumns.join(",") + "\n";
+            
+            aData.forEach(function (oRow) {
+                var aValues = aColumns.map(function (sCol) {
+                    return '"' + (oRow[sCol] || "") + '"';
+                });
+                sCsv += aValues.join(",") + "\n";
+            });
+            
+            var oBlob = new Blob([sCsv], { type: "text/csv;charset=utf-8;" });
+            var sUrl = URL.createObjectURL(oBlob);
+            var oLink = document.createElement("a");
+            oLink.setAttribute("href", sUrl);
+            oLink.setAttribute("download", sFileName + ".csv");
+            oLink.click();
+            
+            MessageToast.show("Exported successfully");
+        },
+
         onRequestPress: function (oEvent) {
             var oItem = oEvent.getSource();
             var oContext = oItem.getBindingContext();
@@ -189,24 +514,33 @@ sap.ui.define([
 
         onRejectRequest: function () {
             if (this._selectedRequest) {
-                var oModel = this.getModel();
-                var aRequests = oModel.getProperty("/requests") || [];
-                
-                var iIndex = aRequests.indexOf(this._selectedRequest);
-                if (iIndex > -1) {
-                    aRequests.splice(iIndex, 1);
-                }
-                
-                oModel.setProperty("/requests", aRequests);
-                
-                this._addNotification(
-                    "System",
-                    "Request from " + this._selectedRequest.employeeName + " for '" + this._selectedRequest.requestedAssetName + "' was rejected",
-                    "sap-icon://decline"
+                this._showConfirmDialog(
+                    "Reject Request",
+                    "Are you sure you want to reject the request from " + this._selectedRequest.employeeName + "?",
+                    "Reject",
+                    "Reject",
+                    "Error",
+                    function () {
+                        var oModel = this.getModel();
+                        var aRequests = oModel.getProperty("/requests") || [];
+                        
+                        var iIndex = aRequests.indexOf(this._selectedRequest);
+                        if (iIndex > -1) {
+                            aRequests.splice(iIndex, 1);
+                        }
+                        
+                        oModel.setProperty("/requests", aRequests);
+                        
+                        this._addNotification(
+                            "System",
+                            "Request from " + this._selectedRequest.employeeName + " for '" + this._selectedRequest.requestedAssetName + "' was rejected",
+                            "sap-icon://decline"
+                        );
+                        
+                        MessageToast.show("Request rejected");
+                        this._requestDetailDialog.close();
+                    }.bind(this)
                 );
-                
-                MessageToast.show("Request rejected");
-                this._requestDetailDialog.close();
             }
         },
 
